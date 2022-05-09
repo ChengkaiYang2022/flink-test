9a8fac53e770941291d49acac0a3a6665b827e02
Chengkai Yang <yangchengkai2022@163.com>
2022-05-09 05:56:48 +0400

    Flink 1.14 Table API & Flink SQL test

---
+++ flink114/src/main/java/com/yck/TestSQL.java
@@ -0,0 +1,29 @@
+package com.yck;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+
+public class TestSQL {
+    public static void main(String[] args) {
+
+        EnvironmentSettings settings = EnvironmentSettings
+                .newInstance()
+                .inStreamingMode()
+                //.inBatchMode()
+
+                .build();
+
+        TableEnvironment tableEnv = TableEnvironment.create(settings);
+        Configuration configuration = tableEnv.getConfig().getConfiguration();
+        configuration.setInteger("table.exec.resource.default-parallelism",1);
+
+        tableEnv.executeSql("CREATE TABLE SourceTable (f0 String) with ('connector' = 'datagen','rows-per-second' = '1')");
+        // WILL WORK WITH (EXCLUDING OPTIONS)
+        tableEnv.executeSql("CREATE TEMPORARY TABLE SinkTable WITH ('connector' = 'print') LIKE SourceTable (EXCLUDING OPTIONS) ");
+        // WILL NOT WORK WITHOUT (EXCLUDING OPTIONS)!
+//        tableEnv.executeSql("CREATE TABLE SinkTable (product STRING) WITH ('connector' = 'print') ");
+
+        tableEnv.executeSql("INSERT INTO SinkTable SELECT f0 FROM SourceTable");
+    }
+}
