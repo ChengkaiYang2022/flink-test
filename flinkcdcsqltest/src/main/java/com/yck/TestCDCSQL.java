345d9333b2c1a2c71655e4b2f12574eb5880222c
Chengkai Yang <yangchengkai2022@163.com>
2022-05-27 20:20:52 +0800

    union all
    Signed-off-by: Chengkai Yang <yangchengkai2022@163.com>

--- flinkcdcsqltest/src/main/java/com/yck/TestCDCSQL.java
+++ flinkcdcsqltest/src/main/java/com/yck/TestCDCSQL.java
@@ -1,10 +1,31 @@
 package com.yck;
 
+import org.apache.flink.calcite.shaded.com.google.common.io.Resources;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableEnvironment;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestCDCSQL {
+    private static final String[] DDL_PATH = {"sql/table.sql",
+            "sql/table_a_1.sql","sql/table_a_2.sql","sql/table_a_3.sql"
+    };
+
+    public static String getTableSql(String path){
+        String strings = "";
+        URL url = Resources.getResource(path);
+        try {
+            strings = Resources.toString(url, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return strings;
+    }
     public static void main(String[] args) {
 
         EnvironmentSettings settings = EnvironmentSettings
@@ -14,25 +35,18 @@ public class TestCDCSQL {
                 .build();
 
         TableEnvironment tEnv = TableEnvironment.create(settings);
-        tEnv.executeSql("CREATE TABLE table_a (\n" +
-                "               id INT NOT NULL,\n" +
-                "                ztid STRING,\n" +
-                "                ztmc STRING,\n" +
-                "                 PRIMARY KEY(id) NOT ENFORCED\n" +
-                ") WITH (\n" +
-                " 'connector' = 'mysql-cdc',\n" +
-                " 'hostname' = '192.168.56.101',\n" +
-                " 'port' = '3306',\n" +
-                " 'username' = 'root',\n" +
-                " 'password' = '123456',\n" +
-                " 'database-name' = 'test1',\n" +
-                " 'table-name' = 'table_a'\n" +
-                ")");
+        for (String path : DDL_PATH) {
+            tEnv.executeSql(getTableSql(path));
+        }
 //        tEnv.executeSql("CREATE TABLE SourceTable (f0 String) with ('connector' = 'datagen','rows-per-second' = '1')");
 
         tEnv.executeSql("CREATE TEMPORARY TABLE SinkTable WITH ('connector' = 'print') LIKE table_a (EXCLUDING ALL) ");
 
-        tEnv.executeSql("INSERT INTO SinkTable SELECT * FROM table_a");
+        tEnv.executeSql("INSERT INTO SinkTable SELECT * FROM table_a"
+                        + " UNION ALL SELECT * FROM table_a_1"
+                        + " UNION ALL SELECT * FROM table_a_2"
+                +" UNION ALL SELECT * FROM table_a_3"
+                );
 //        tEnv.sqlQuery("select * from SinkTable").printSchema();
 //        env.execute();
 
---
+++ flinkcdcsqltest/target/classes/sql/table_a_3.sql
@@ -0,0 +1,14 @@
+CREATE TABLE table_a_3 (
+               id INT NOT NULL,
+                ztid STRING,
+                ztmc STRING,
+                 PRIMARY KEY(id) NOT ENFORCED
+) WITH (
+ 'connector' = 'mysql-cdc',
+ 'hostname' = '192.168.56.101',
+ 'port' = '3306',
+ 'username' = 'root',
+ 'password' = '123456',
+ 'database-name' = 'test1',
+ 'table-name' = 'table_a_3'
+)
